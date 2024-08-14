743b45c8ae8d7f4acd4e069b03e076f09a00f703
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-08-14 18:26:21 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -93,7 +93,7 @@ async def private_receive_handler(c: Client, m: Message):
             return
 
     # New functionality for Terabox links
-    if m.text and "terabox.com" in m.text:
+    if m.text and "teraboxapp.com" in m.text:
         await c.send_chat_action(chat_id=m.chat.id, action="typing")
         ms = m.text
         url = f"https://teraboxvideodownloader.nepcoderdevs.workers.dev/?url={ms}"
